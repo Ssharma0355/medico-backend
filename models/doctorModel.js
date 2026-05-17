fd52d8e9f79dda9c3da82699c3f73f6b1a9f9f6a
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-05-17 19:17:40 +0530

    doctor model and schema

---
+++ models/doctorModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    speciality: { type: String, required: true },
+    degree: { type: String, required: true },
+    experience: { type: String, required: true },
+    about: { type: String, required: true },
+    avaiable: { type: Boolean, required: true },
+    fee: { type: Number, required: true },
+    address: { type: Object, required: true },
+    date: { type: Number, required: true },
+    slots_booked: { type: Object, default: {} }
+}, { minmize: false })
+
+const doctorModel = mongoose.model.doctor || mongoose.model('doctor',doctorSchema)
+
+export default doctorModel
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import { connectDb } from "./config/mongodb.js"
+import { connectCloudinary } from "./config/cloudinary.js"
 
 
 // App Config
@@ -9,6 +10,7 @@ const app = express()
 const port = process.env.PORT || 8080
 
 connectDb();
+connectCloudinary();
 
 // Middleware
 
